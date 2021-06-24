b21c237e56ad59a75d339bd52de96c32df6941d3
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-06-24 18:26:07 -0300

    authentication system done

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-   var _isAuthenticated = false;
-   var _user;
+  UserModel? _user;
 
-   get user => _user;
+  UserModel get user => _user!;
 
-   void setUser(BuildContext context, var user) {
-     if(user != null) {
-       _user = user;
-       _isAuthenticated = true;
-       Navigator.pushReplacementNamed(context, "/home");
-     } else {
-       _isAuthenticated = false;
-       Navigator.pushReplacementNamed(context, "/login");
-     }
-   }
+  void setUser(BuildContext context, UserModel? user) {
+    if(user != null) {
+      saveUser(user);
+      _user = user;
+      Navigator.pushReplacementNamed(context, "/home");
+    } else {
+      Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(Duration(seconds: 2));
+    if(instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
+    }
+  }
  }
