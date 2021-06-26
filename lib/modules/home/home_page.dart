5c9e9389ffd4556af941cb20eb7f6a26dbdfcd1e
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-06-26 11:27:34 -0300

    adding camera widget, fake InsertBoleto page and listener for InsertBoleto navigate

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -19,90 +19,92 @@ class _HomePageState extends State<HomePage> {
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(152),
-        child: Container(
-          height: 152,
-          color: AppColors.primary,
-          child: Center(
-            child: ListTile(
-              title: Text.rich(
-                TextSpan(
-                  text: "Olá, ",
-                  style: TextStyles.titleRegular,
-                  children: [
-                    TextSpan(
-                      text: "Jovino",
-                      style: TextStyles.titleBoldBackground,
-                    ),
-                  ]),
-              ),
-              subtitle: Text(
-                "Mantenha suas contas em dia",
-                style: TextStyles.captionShape,
-              ),
-              trailing: Container(
-                height: 48,
-                width: 48,
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.circular(5),
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(152),
+          child: Container(
+            height: 152,
+            color: AppColors.primary,
+            child: Center(
+              child: ListTile(
+                title: Text.rich(
+                  TextSpan(
+                    text: "Olá, ",
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                        text: "Jovino",
+                        style: TextStyles.titleBoldBackground,
+                      ),
+                    ]),
+                ),
+                subtitle: Text(
+                  "Mantenha suas contas em dia",
+                  style: TextStyles.captionShape,
+                ),
+                trailing: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                 ),
               ),
             ),
           ),
         ),
-      ),
-      body: pages[homeController.currentPage],
-      bottomNavigationBar: Container(
-        height: 90,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              onPressed: (){
-                homeController.setPage(0);
-                setState(() {
-                  
-                });
-              },
-              icon: Icon(
-                Icons.home,
-                color: AppColors.primary,
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Navigator.pushNamed(context, "/barcode_scanner");
-              },
-              child: Container(
-                height: 56,
-                width: 56,
-                decoration: BoxDecoration(
+        body: pages[homeController.currentPage],
+        bottomNavigationBar: Container(
+          height: 90,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: (){
+                  homeController.setPage(0);
+                  setState(() {
+                    
+                  });
+                },
+                icon: Icon(
+                  Icons.home,
                   color: AppColors.primary,
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Icon(
-                  Icons.add_rounded,
-                  color: AppColors.background,
                 ),
               ),
-            ),
-            IconButton(
-              onPressed: (){
-                homeController.setPage(1);
-                setState(() {
-                  
-                });
-              },
-              icon: Icon(
-                Icons.description_outlined,
-                color: AppColors.body,
+              GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(context, "/barcode_scanner");
+                },
+                child: Container(
+                  height: 56,
+                  width: 56,
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    Icons.add_rounded,
+                    color: AppColors.background,
+                  ),
+                ),
               ),
-            ),
-          ],
-        )
+              IconButton(
+                onPressed: (){
+                  homeController.setPage(1);
+                  setState(() {
+                    
+                  });
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                ),
+              ),
+            ],
+          )
+        ),
       ),
     );
   }
