f724ee7d53149da4a2f71895919f3ac85705af47
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-06-27 07:07:50 -0300

    Adding MeusBoletos page and BoletoInfo widget

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/modules/home/home_controller.dart';
+import 'package:payflow/modules/meus_boletos/meus_boletos_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_style.dart';
-import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -15,9 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final homeController = HomeController();
   final pages = [
-    Container(
-      child: BoletoListWidget(),
-    ),
+    MeusBoletosPage(),
     Container(
       color: Colors.blueGrey,
     ),
