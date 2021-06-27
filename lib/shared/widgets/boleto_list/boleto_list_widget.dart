cd7d8f55d85bbec0c1eaa82761fff010ac507a4d
Jovino Esteban Guerrero Toledo <CS333972@MinhaTI.com.br>
2021-06-27 01:25:43 -0300

    Adding getBoletos()

--- lib/shared/widgets/boleto_list/boleto_list_widget.dart
+++ lib/shared/widgets/boleto_list/boleto_list_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
 import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
 import 'package:payflow/shared/widgets/boleto_tile/boleto_tile_widget.dart';
 
@@ -13,8 +14,11 @@ class _BoletoListWidgetState extends State<BoletoListWidget> {
   final controller = BoletoListController();
   @override
   Widget build(BuildContext context) {
-    return Column(children:
-      controller.boletos.map((e) => BoletoTileWidget(data: e)).toList(),
+    return ValueListenableBuilder<List<BoletoModel  >>(
+      valueListenable: controller.boletosNotifier,
+      builder: (_,boletos,__) => Column(
+        children: boletos.map((e) => BoletoTileWidget(data: e)).toList(),
+      )
     );
   }
 }
